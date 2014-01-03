f6e8820aee3a2db8a7109d71dbd295ab014a1dc3
dcods22 <daniel.cody2@marist.edu>
2014-01-03 00:49:42 -0500

    Added Angular Controller

--- about.php
+++ about.php
@@ -1,19 +1,10 @@
 <?php
 	include('lib/template/head.php');
 ?> 
+	<div data-ng-controller='AboutController'>
 	
-	<div ng-init=" 
-		
-		honors = [{name:'Eagle Scout from Boy Scouts of America'}, {name:'150+ Hours Community Service'}, {name:'DECA All Long Island Award Winner in Marketing Mathematics (2011)'}, {name:'All League Athlete: Soccer, and Baseball'}, {name:'Varsity Captain of Soccer: 09, 10'}, {name:'Varsity Captain of Baseball: 11'}];
-		
-		skills = ['Java', 'HTML5', 'CSS3', 'PHP', 'MySQL', 'JavaScript', 'AngularJS ', 'C++', 'jQuery', 'AngularJS', 'Git', 'z/OS', 'Mainframe', 'REXX', 'TSO', 'ISPF', 'IPCS', 'SML', 'Assembly', 'Mac OS', 'Windows OS', 'Linux', 'IOS'];
-		
-		interests = ['Sports', 'Beach', 'Reading', 'Working Out'];
-	
-		activities = ['Circle K Society', 'Computer Society', 'Security Club', 'Campus Ministry', 'Intramurals', 'Soccer', 'Baseball', 'Basketball', 'Hockey'];
-	
-		">
-	</div>
+	<script src='scripts/javascript/aboutController.js' type='text/javascript'></script>
+
 	<br/>
 	<div class='searchBar'>Search: <input type='text' name='search' class='searchAbout' ng-model='search' placeholder='Search About Me' /></div>
 	
@@ -36,7 +27,7 @@
 		<ul class='subjectHolder'>
 		    <li ng-repeat="activity in activities | filter:search" class='aboutSubject'>{{activity}}</li>
 		</ul>
-
+	</div>
 <?php
 	include('lib/template/footer.php');
 ?>
--- lib/template/head/head.php
+++ lib/template/head/head.php
@@ -6,11 +6,11 @@
 		<link href="CSS/template.css" rel="stylesheet" type="text/css"/>
 		<link rel="shortcut icon" href="images/favicon.ico"/>
 		<link rel="icon" href="images/favicon.ico"/>
-		<script src="scripts/jquery.js"></script>		
-		<script src="scripts/jquery-ui.js"></script>
-		<script src="scripts/accordian.js"></script>		
+		<script type ="text/javascript"src="scripts/jquery.js"></script>		
+		<script type="text/javascript" src="scripts/jquery-ui.js"></script>
+		<script type="text/javascript" src="scripts/accordian.js"></script>		
 		<script type="text/javascript" src="scripts/carousel/js/infinitecarousel.js"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script src="scripts/angular/angular.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/angular/angular.js"></script>
 		<title>Dan Cody</title>
 	</head>
