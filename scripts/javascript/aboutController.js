b7679ab7f7a0e06d862f8ff593501cbb490c7e07
dcods22 <daniel.cody2@marist.edu>
2014-01-03 20:51:43 -0500

    added angular includes

---
+++ scripts/javascript/aboutController.js
@@ -0,0 +1,11 @@
+function AboutController($scope){
+			
+	$scope.honors = [{name:'Eagle Scout from Boy Scouts of America'}, {name:'150+ Hours Community Service'}, {name:'DECA All Long Island Award Winner in Marketing Mathematics (2011)'}, {name:'All League Athlete: Soccer, and Baseball'}, {name:'Varsity Captain of Soccer: 09, 10'}, {name:'Varsity Captain of Baseball: 11'}];
+		
+	$scope.skills = ['Java', 'HTML5', 'CSS3', 'PHP', 'MySQL', 'JavaScript', 'AngularJS ', 'C++', 'jQuery', 'AngularJS', 'Git', 'z/OS', 'Mainframe', 'REXX', 'TSO', 'ISPF', 'IPCS', 'SML', 'Assembly', 'Mac OS', 'Windows OS', 'Linux', 'IOS'];
+		
+	$scope.interests = ['Sports', 'Beach', 'Reading', 'Working Out'];
+	
+	$scope.activities = ['Circle K Society', 'Computer Society', 'Security Club', 'Campus Ministry', 'Intramurals', 'Soccer', 'Baseball', 'Basketball', 'Hockey'];
+
+}
