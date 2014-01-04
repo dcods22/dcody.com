c34e5286e23d797e14b34b6c57bcfe00f10e4ccf
dcods22 <daniel.cody2@marist.edu>
2014-01-03 23:58:46 -0500

    Updated to HTML Server Side Includes

--- scripts/javascript/aboutController.js
+++ scripts/javascript/aboutController.js
@@ -1,4 +1,6 @@
-function AboutController($scope){
+var aboutController = angular.module('aboutController', []);
+
+aboutController.controller('AboutController', function AboutController($scope){
 			
 	$scope.honors = [{name:'Eagle Scout from Boy Scouts of America'}, {name:'150+ Hours Community Service'}, {name:'DECA All Long Island Award Winner in Marketing Mathematics (2011)'}, {name:'All League Athlete: Soccer, and Baseball'}, {name:'Varsity Captain of Soccer: 09, 10'}, {name:'Varsity Captain of Baseball: 11'}];
 		
@@ -8,4 +10,4 @@ function AboutController($scope){
 	
 	$scope.activities = ['Circle K Society', 'Computer Society', 'Security Club', 'Campus Ministry', 'Intramurals', 'Soccer', 'Baseball', 'Basketball', 'Hockey'];
 
-}
+});
