6143494b97f0e66dbdf26cf3d9367d8c837cd764
dcods22 <daniel.cody2@marist.edu>
2014-01-06 12:28:12 -0500

    AngularJS resume

--- resume.php
+++ resume.php
@@ -10,13 +10,13 @@
 		<script src='scripts/javascript/resumeController.js' type='text/javascript'></script>
 
 	
-		<p> Here is a downloadable, better formatted file of my Resume: <a href = "Resume.docx">Click Here</a></p>
+		Here is a downloadable, better formatted file of my Resume: <a href = "Resume.docx">Click Here</a>
 		
-		<p>
+		<br/><br/>
 		
-		<center><strong>Daniel E. Cody</strong></center>
+		<span class='resName'><center>Daniel E. Cody</center></span>
 		
-			<center>43 Watson Avenue, West Babylon, New York 11704 : Daniel.Cody2@Marist.edu  :	631-365- 9940 </center><br/>
+			<center>43 Watson Avenue, West Babylon, New York 11704 <br/> Daniel.Cody2@Marist.edu  :	631-365- 9940 </center><br/>
 
 			<hr>
 
@@ -25,102 +25,34 @@
 			
 				<u>{{edu.name}}</u><br/>
 				{{edu.info}}
-				<br/>{{edu.gpa}}<br/>
+				<br/>{{edu.gpa}}
 				
 			</div>
 			
-			<br/><hr><strong>SKILLS</strong><br/>
+			<hr><strong>SKILLS</strong><br/>
 			
 			<div ng-repeat="skill in skills">
 				<u>{{skill.name}}</u>
 				<br/>{{skill.info}}<br/>
 			</div>
 
-			<br/><hr><strong>RELEVANT EXPERIENCE</strong><br/>
-			<u>IBM Level 2 z/OS Co-op	Poughkeepsie, NY
-			Programmer	Summer 2013</u>
-			<br/>Implemented, developed, maintained, and updated REXX, Java, and Lotus tools
-			<br/>Used the mainframe to run and update code, as well as debugged mainframe dumps using REXX tools
-			<br/>Part of an intern team which worked on various projects to help Level 2 debuggers 
-
-			<br/><br/>
+			<hr><strong>RELEVANT EXPERIENCE</strong><br/>
 			
-			<u>CampusLoops LLC	West Babylon, NY
-			CEO and Programmer	Fall 2013-Current
-			</u>
-			<br/>Designed, planned and managed a website startup company<br/>
-			Currently beta testing with expected test launch to Marist College in late Fall 2013 
-
-			<br/><br/>
-			
-			<u>Resnet  	Poughkeepsie NY
-			Networking Technician	Fall 2013-Current
-			</u>
-			<br/>Implemented and install VPN programs to establish connection to the Marist Network
-			Provide technical support to the students, faculty and staff of Marist College<br/>
-			Solve software and hardware related issues using the command prompt
-
-
-			
-			<br/><br/><u>KrowdByz Development Intern	Seattle, Washington
-			Developer and Programmer	Winter 2012-Spring 2013</u>
-			<br/>Developed and scripted PHP tools using PHP Curl for other companies and websites.  Grabbed data, as well as did website functions using our scripts hosted on our own servers
-
-			<br/><br/><u>Web Developer Intern at TTNG	Queens, NY
-			Developer and Programmer	Winter 2012-Spring 2013</u>
-			<br/>Helped develop and plan the front end of the website with HTML, CSS and JavaScript; as well as the 
-			backend of the website for user login and other features, with PHP, and MySQL.
-			
-			<br/><br/><u>Marist Institute of Public Opinion	Poughkeepsie, NY
-			Interviewer and Researcher	   Spring 2012-Winter 2012</u>
-			<br/>	Researcher calling people to answer polls and other surveys to help gather demographic information about 
-			issues that face people in their local communities
-			<br/>	Form statistics that are used by The Wall Street Journal, MSNBC, NBC, and many other political institutions<br/>
-			
-			<br/><u>Robert Moses State Park	Babylon NY
-			Supervisor of Tolls Department	Summer 2012</u>
-			<br/>	Supervised Tolls Department at Robert Moses State Park including recording and accounting for daily park 
-			revenues which included up to $45,000 cash on a daily basis
-			<br/>	Supported performance of toll collectors in their day to day activities, while handling any problems in which occurred
-			Park and Recreation Aide											Summer 2010, 2011
-			<br/>	Collected parking tolls for admission into the park, addressed customer questions and concerns
-			<br/>	Collaborated with other aides to keep the park facilities clean and running properly<br/> 
+			<div class="experience" ng-repeat="exp in experience">
+				<strong>{{exp.company}}</strong><span style='float:right'>{{exp.location}}</span><br/>
+				<u>{{exp.title}}</u><span style='float:right'>{{exp.time}}</span>
+				
+				<div>{{exp.bullet1}} </div>
+				<div>{{exp.bullet2}} </div>
+				<div>{{exp.bullet3}}</div>
+				
+			</div>
 		
-			<br/><u>Computer Society	    Poughkeepsie, NY
-			Member	 Fall 2011-Current</u>
-			<br/>	Discuss relevant technology issues and new forms of technology that are impacting the industry, including 
-			potential ideas such as Java in the real world and comparisons of operating systems
-			<br/>	Attended events including: guest speakers from local companies and also former Marist graduates, programming 
-			and hack nights<br/>
-			
-			<br/><u>West Babylon Community Youth Center 	 West Babylon, NY 
-			Referee, Clinic Leader	Winters 2009- 2011</u>
-			<br/>	Refereed games of children, ensuring that they understood the rules of Basketball 
-			<br/>	Coordinated with other high school players to lead clinics that teach younger players the basic skills of
-			basketball, while promoting the importance of teamwork and building leadership skills<br/> 
-			
-			<br/><u>Marist College Campus Ministry										Poughkeepsie, NY 
-			Volunteer											               Fall 2011- Current</u>
-			<br/>	Volunteer for many community service projects such as: Relay for Life, The Hunger Walk, and other local 
-			community service
-			
-			<br/><br/><u>Marist College Circle K Society								        		Poughkeepsie, NY
-			Volunteer												Fall 2011- Current</u>
-			<br/>	Perform community service on campus and in the local community through running blood drives, working at
-			food banks, and helping other local charities<br/>
-			
-			<br/><u>Babylon High School Varsity Soccer & Baseball Teams                                                      Babylon, NY
-			Captain	Fall 2009- Spring 2011</u>
-			<br/>	Lead the team through warm up practice drills, and captains practices
-			<br/>	Be a sense of support to other team members and promote an environment of the team learning from each other 
-			<br/>
-
-			
-			<br/><hr><strong>HONORS AND ACHIEVEMENTS</strong><br/>
+			<hr><strong>HONORS AND ACHIEVEMENTS</strong><br/>
 			<div ng-repeat="honor in honors">
 				<strong>{{honor.name}}</strong> {{honor.info}}<br/>
 			</div>
-		</p>
+		
 	   
 	<?php
 		include('lib/template/footer.html');
--- scripts/javascript/resumeController.js
+++ scripts/javascript/resumeController.js
@@ -8,33 +8,134 @@ resumeController.controller('ResumeController', function ResumeController($scope
 			
 			info: 'Bachelor of Science in Computer Science, Minor in Information Technology 	Expected Graduation: May 2015',
 		
-			gpa: 'Major GPA: 3.41/4.00; Cumulative GPA: 3.21/4.00'
-		}
+			gpa: 'Major GPA: 3.41/4.00; Cumulative GPA: 3.21/4.00',
+		},
 	];
 	
 	$scope.skills = [
 		{
 			name: 'Relevant Classes:',
 			
-			info: 'Computer Science I & II, Web Programming I & II, Language Study, C++ Programming, Assembly Language Programming, IT\ITS Concepts, Logic Design, Data Communications, Advanced Data Structures, Enterprise Computing, Theory of Programming Languages, Internetworking, Software Development'
-			
+			info: 'Computer Science I & II, Web Programming I & II, Language Study, C++ Programming, Assembly Language Programming, IT\ITS Concepts, Logic Design, Data Communications, Advanced Data Structures, Enterprise Computing, Theory of Programming Languages, Internetworking, Software Development',
+	
 		},
 		
 		{
 			name: 'Technical Skills:',
 			
-			info: 'Experienced with Windows, Linux, Mac, and IOS Operating Systems; Knowledge in Java, HTML5, CSS3, PHP, MySQL, Mainframe, C++,  JavaScript, jQuery, AngularJS, AJAX, z/OS, ISPF, TSO, Git, IPCS, XML, SML, REXX and Assembly Language'
+			info: 'Experienced with Windows, Linux, Mac, and IOS Operating Systems; Knowledge in Java, HTML5, CSS3, PHP, MySQL, Mainframe, C++,  JavaScript, jQuery, AngularJS, AJAX, z/OS, ISPF, TSO, Git, IPCS, XML, SML, REXX and Assembly Language',
 			
-		}
+		},
 		
 	];
 
 	$scope.experience = [
 		{
-			name: 'IBM Level 2 z/OS Co-op	Poughkeepsie, NY Programmer	Summer 2013',
+			company: 'IBM',
 			
+			location: 'Poughkeepsie, NY',
 			
-		}
+			time: 'Summer 2013',
+			
+			title: 'Level 2 z/OS Co-op Programmer',
+			
+			bullet1: 'Implemented, developed, maintained, and updated REXX, Java, and Lotus tools',
+			
+			bullet2: 'Used the mainframe to run and update code, as well as debugged mainframe dumps using REXX tools',
+			
+			bullet3: 'Part of an intern team which worked on various projects to help Level 2 debuggers ',
+		
+		},
+		
+		{
+			company: 'CampusLoops LLC',
+			
+			location: 'West Babylon, NY',
+			
+			time: 'Fall 2013-Current',
+			
+			title: 'CEO and Programmer',
+			
+			bullet1: 'Designed, planned and managed a website startup company',
+			
+			bullet2: 'Currently beta testing with expected test launch to Marist College in late Fall 2013 ',
+			
+		},
+		
+		{
+			company: 'Resnet',
+			
+			location: 'Poughkeepsie, NY',
+			
+			time: 'Fall 2013-Current',
+			
+			title: 'Networking Technician',
+			
+			bullet1: 'Implemented and install VPN programs to establish connection to the Marist Network',
+			
+			bullet2: 'Provide technical support to the students, faculty and staff of Marist College',
+			
+			bullet3: 'Solve software and hardware related issues using the command prompt',
+		},
+		
+		{
+			company: 'KrowdByz',
+			
+			location: 'Seattle, Washington',
+			
+			time: 'Winter 2012-Spring 2013',
+			
+			title:'Developer and Programmer Intern',
+			
+			bullet1: 'Developed and scripted PHP tools using PHP Curl for other companies and websites.  ',
+			
+			bullet2: 'Grabbed data, as well as did website functions using our scripts hosted on our own servers',
+		},
+		
+		{
+			company: 'TTNG',
+			
+			location: 'Queens, NY',
+			
+			time: 'Winter 2012-Spring 2013',
+			
+			title: 'Developer and Programmer Intern',
+			
+			bullet1: 'Created and planed the front end of the website with HTML, CSS and JavaScript',
+			
+			bullet2: 'Coded the backend of the website with PHP and MySQL to create a user login and other features',
+			
+		},
+		
+		{
+			company: 'Marist Institute of Public Opinion',
+			
+			location: 'Poughkeepsie, NY',
+			
+			time: 'Spring 2012-Winter 2012',
+			
+			title: 'Interviewer and Researcher',
+			
+			bullet1: 'Researcher calling people to answer polls and other surveys to help gather demographic information about issues that face people in their local communities',
+			
+			bullet2: 'Form statistics that are used by The Wall Street Journal, MSNBC, NBC, and many other political institutions',
+			
+		},
+		
+		{
+			company: 'Robert Moses State Park',
+			
+			location: 'Babylon, NY',
+			
+			time: 'Summer 2012',
+			
+			title: 'Supervisor of Tolls Department',
+			
+			bullet1: 'Supervised Tolls Department at Robert Moses State Park which included recording and accounting for daily park revenues of up to $45,000 per day',
+			
+		},
+		
+		
 	
 	];
 	
@@ -42,21 +143,21 @@ resumeController.controller('ResumeController', function ResumeController($scope
 		{
 			name: 'Eagle Scout Award Recipient: ', 
 		
-			info: 'Highest achievement in Boy Scouts of America'
+			info: 'Highest achievement in Boy Scouts of America',
 		},
 		
 		{
 			name: '',
 			
-			info: 'Community Service +150 Hours Annually: Boy Scout events, Good Samaritan Hospital, American Legion Post 94'
+			info: 'Community Service +150 Hours Annually: Boy Scout events, Good Samaritan Hospital, American Legion Post 94',
 		
 		},
 		
 		{
 			name: '',
 		
-			info: 'DECA All Long Island Award Winner in Marketing Mathematics (2011)'
-		}
+			info: 'DECA All Long Island Award Winner in Marketing Mathematics (2011)',
+		},
 	];
 	
 });
